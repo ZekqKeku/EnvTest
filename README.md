f8f7e0a4c9cce737151a9dca8bd88069ed4012ee
ZekqKeku <alekschojnowski@gmail.com>
2026-06-25 13:17:20 +0200

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# EnvTest
+
+Prosty projekt  stworzony w celu nauki pracy ze zmiennymi środowiskowymi (`.env`) oraz GitHub Actions z Python.
+
---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+import customtkinter as ctk
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.getenv("API_KEY", "Brak klucza w pliku .env")
+
+ctk.set_appearance_mode("System")
+ctk.set_default_color_theme("blue")
+
+app = ctk.CTk()
+app.geometry("400x200")
+app.title("Aplikacja Env Test")
+
+label = ctk.CTkLabel(app, text=f"Wiadomość:\n{api_key}", font=("Arial", 16))
+label.pack(expand=True, pady=20)
+
+close_button = ctk.CTkButton(app, text="Zamknij", command=app.destroy)
+close_button.pack(side="bottom", pady=20)
+
+if __name__ == "__main__":
+    app.mainloop()
